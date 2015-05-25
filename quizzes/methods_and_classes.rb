3eeb359fd6349f9d5512eb3b49221f892cea7fbf
SUSANAYEPES <SYEPES@BU.EDU>
2015-05-25 14:46:21 +0200

    Uploading my 2nd quiz

---
+++ quizzes/methods_and_classes.rb
@@ -0,0 +1,72 @@
+Q1:
+	method: my name
+	return value: "Zoo lander"
+
+Q2: nil
+
+
+Q3: 
+def silly_check (number)
+		if number > 5
+		 "The number is less than 5."
+		else
+			"The number is greater than or equal to 5."
+		end
+end
+
+Q4: def funify (array)
+		array << "fun"
+	end
+
+
+Q5: 
+def more_fun 
+end
+
+Q6:
+class Dog
+	def initialize (name)
+	@name = name
+	end
+
+	def bark
+		"Ruff Ruff"
+	end
+end 
+
+spot = Dog.new ("Ruff, Ruff")
+
+
+Q7:
+Error...undefined method for speak
+
+
+Q8: 
+class Insect
+  def initialize(age_in_days)
+    @age_in_days = age_in_days
+  end
+
+  def age_in_year
+  	"#{age_in_days}" / 365 
+  end
+end
+
+Q9: 
+class Person
+	def initialize(age)
+		@age = age
+	end
+
+	def age = input
+		@age = input
+	end
+
+end	
+
+Q10: 
+class Person
+	attr_accessor :age
+end
+
+
